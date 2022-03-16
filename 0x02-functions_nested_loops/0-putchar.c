9628494aef92960d1ba01fbced0929f3881797c1
Callmesarah <siamorgann@gmail.com>
2022-03-16 05:11:56 +0100

    new commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - Entry point
+* write a program that prints Holberton, followed by a new line.
+* Return: Always (Success)
+*/
+int main(void)
+{
+	char *ch = "_putchar";
+
+	while (*ch)
+	{
+		_putchar(*ch);
+		ch++;
+	}
+	_putchar('\n');
+return (0);
+}
+
