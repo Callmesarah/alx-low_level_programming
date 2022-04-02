21c66b3a8bff6f036952ff1fd1a685c47b653d43
Callmesarah <csiamorgamn@gmail.com>
2022-04-02 05:17:16 +0100

    a new commit

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,16 +1,11 @@
 #include "main.h"
+
 /**
-*_isupper - function that checks for uppercase characters
-*@c: int type number
-*Return: 1 if uppercase, else 0
-*/
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
 {
-
-	if ((c >= 'A') && (c <= 'Z'))
-
-	return (1);
-	else
-
-	return (0);
+	return (c >= 'A' && c <= 'Z');
 }
